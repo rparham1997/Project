a8ae0ec6d634726f3e88f517659ae83da96959aa
philparham <rparham1997@gmail.com>
2024-05-06 19:39:59 -0400

    Sample Project

---
+++ Project/Services/SavedOrderResponse.swift
@@ -0,0 +1,8 @@
+//
+//  SavedOrderResponse.swift
+//  Project
+//
+//  Created by Ramar Parham on 5/6/24.
+//
+
+import Foundation
---
+++ Project/Views/AddCoffeeOrderView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCoffeeOrderView.swift
+//  Project
+//
+//  Created by Ramar Parham on 5/6/24.
+//
+
+import SwiftUI
+
+struct AddCoffeeOrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCoffeeOrderView()
+}
