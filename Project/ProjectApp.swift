edb3f30e508db1664a040684cc62c4fc313236dc
philparham <rparham1997@gmail.com>
2024-05-06 18:04:47 -0400

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Ramar Parham on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
